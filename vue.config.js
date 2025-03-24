cd247d6e2fefbc5385efe1745e6418c70250bbf2
jdhantianxu@163.com <jdhantianxu@163.com>
2025-03-24 20:33:08 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        client: {
+            overlay: false
+        }
+    }
 })
 module.exports = {
     css: {
